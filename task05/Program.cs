d8f65f08c25ff7633223657d8d80fc98e55ad8ff
Slava <berestevich1998@mail.rum>
2023-08-06 14:05:30 +0300

    первая задача

---
+++ task05/Program.cs
@@ -0,0 +1,12 @@
+﻿/* 07. # Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+*/
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>99 && number<1000)
+{
+    Console.WriteLine(number%10);
+}
+else 
+{
+    Console.WriteLine("число не трехзначное");
+}
---
+++ task15/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+Console.WriteLine("Введите номер дня недели от 1 до 7: ");
+int weekday = Convert.ToInt32(Console.ReadLine());
+if (weekday == 1)
+{
+    Console.WriteLine("Понедельник");
+    Console.WriteLine("не выходной");
+}
+else if (weekday == 2)
+{
+    Console.WriteLine("Вторник");
+    Console.WriteLine("не выходной");
+}
+else if (weekday == 3)
+{
+    Console.WriteLine("Среда");
+    Console.WriteLine("не выходной");
+}
+else if (weekday == 4)
+{
+    Console.WriteLine("Четверг");
+    Console.WriteLine("не выходной");
+}
+else if (weekday == 5)
+{
+    Console.WriteLine("Пятница");
+    Console.WriteLine("не выходной, но уже скоро");
+}
+else if (weekday == 6)
+{
+    Console.WriteLine("Суббота");
+    Console.WriteLine("выходной");
+}
+else
+{
+    Console.WriteLine("Воскресенье");
+    Console.WriteLine("выходной");
+}
+
+
