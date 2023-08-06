60bc7743e91888629c472811fb65c4d5eddff638
Slava <berestevich1998@mail.rum>
2023-08-06 17:45:57 +0300

    12 задача

---
+++ task14/Program.cs
@@ -0,0 +1,15 @@
+﻿/* 12 Напишите программу которая принимает на вход число и проверяет,
+кратно ли оно одновременно 7 и 23
+*/
+Console.WriteLine("введите число");
+int usernumber = Convert.ToInt32(Console.ReadLine());
+int number1 = 7;
+int number2 = 23;
+if (usernumber % (number1 * number2) == 0)
+{
+    Console.WriteLine($"Число {usernumber} - кратно {number1} и {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {usernumber} - не кратно {number1} и {number2}");
+}
