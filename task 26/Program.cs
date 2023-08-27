0ceba330b6f36d4b8a8738cdb54fdb6a2bda172b
Slava <berestevich1998@mail.rum>
2023-08-27 11:33:51 +0300

    26 задача

---
+++ task 26/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает число и выдает
+количество цифр в числе
+*/
+Console.WriteLine("введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int Quantity (int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        count ++;
+        number = number / 10; 
+    }
+    return count;
+}
+Console.WriteLine(Quantity(userNumber));
