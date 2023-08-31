7b732edf96d82a34d45e5068051927c7265d644e
Slava <berestevich1998@mail.rum>
2023-08-31 19:19:27 +0300

    29 задача

---
+++ task29/Program.cs
@@ -0,0 +1,35 @@
+﻿/*  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+*/
+Console.WriteLine("Введите длину массива и границы рандомных чисел: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[] GetRandomArray(int arrayLength1, int start, int end)
+{
+int[] array = new int[arrayLength1];
+for (int i=0; i<array.Length; i++)
+{
+array [i] = new Random().Next(start, end);
+}
+return array;
+}
+
+int[] userArray = GetRandomArray(arrayLength, min, max);
+
+void PrintArray(int [] arrayToPrint)
+{
+Console.Write("[");
+for (int i=0; i<arrayToPrint.Length; i++)
+{
+Console.Write(arrayToPrint[i]);
+if (i<arrayToPrint.Length - 1)
+{
+Console.Write (", ");
+}
+}
+Console.Write("]");
+}
+
+PrintArray(userArray);
