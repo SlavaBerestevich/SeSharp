6e8ae4f421c4c2159a73e3cb6570c432c1480528
Slava <berestevich1998@mail.rum>
2023-08-31 18:38:00 +0300

    27 задача

---
+++ task27/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая принимает на вход число и выдает сумму чисел в нем
+*/
+Console.WriteLine("введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int SumOfNumber(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+Console.WriteLine($"Сумма чисел в числе {userNumber} равна {SumOfNumber(userNumber)}");
